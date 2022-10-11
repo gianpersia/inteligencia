80aca064c79a4f83f2e7ff72b4160491ef9debdb
gianpersia <g.persia@alumno.um.edu.ar>
2022-10-11 12:08:43 -0300

    update

---
+++ tp1/8puzzle_terminado.py
@@ -0,0 +1,168 @@
+import os
+import random
+import time
+
+lista=[]
+
+#a continuacion defino los movimientos que puede hacer
+
+def izquierda(puzzle, indice):
+    if indice % 3 > 0:
+        auxiliar=puzzle[indice-1]
+        puzzle[indice-1]=puzzle[indice]
+        puzzle[indice]=auxiliar
+        return puzzle
+
+def derecha(puzzle, indice):
+    if indice % 3 < 2:
+        auxiliar=puzzle[indice+1]
+        puzzle[indice+1]=puzzle[indice]
+        puzzle[indice]=auxiliar
+        return puzzle
+
+def arriba(puzzle, indice):
+    if indice - 3 >= 0:
+        auxiliar=puzzle[indice-3]
+        puzzle[indice-3]=puzzle[indice]
+        puzzle[indice]=auxiliar
+        return puzzle
+
+def abajo(puzzle, indice):
+    if indice + 3 < len(puzzle):
+        auxiliar=puzzle[indice+3]
+        puzzle[indice+3]=puzzle[indice]
+        puzzle[indice]=auxiliar
+        return puzzle
+
+#aca se procede a establecer el menu
+def menu_usuario(puzzle):
+    os.system('clear')
+    print("8-PUZZLE\n")
+    print("ESTADO INICIAL:\n")
+    print(f'{puzzle}\n')
+    print("SELECCIONE UN MÉTODO: \n")
+    print("1. MEZCLAR para mezclar 50 veces\n")
+    print("2. RANDOM para realizar busqueda random\n")
+    print("3. ANCHURA para realizar una busqieda por anchura\n")
+    print("4. Terminar el programa.\n")
+    algoritmo = int(input("SELECCIÓN: "))
+    return algoritmo
+
+#esta funcion es para mezclar el puzzle
+
+def mezcla(puzzle):
+    lista1=[0, 1, 2, 3]
+    indice=puzzle.index(0)
+    funcion=random.choice(lista1)
+    if funcion == 2:
+        if indice%3>0:
+            puzzle=izquierda(puzzle, indice)
+    elif funcion==3:
+        if indice%3<2:
+            puzzle=derecha(puzzle, indice)
+    elif funcion==0:
+        if indice-3>=0:
+            puzzle=arriba(puzzle, indice)
+    elif funcion==1:
+        if indice+3<len(puzzle):
+            puzzle=abajo(puzzle, indice)
+    return puzzle
+
+#ahora vamos a definir las funciones de busqueda
+
+def anchura(puzzle, correcto):
+    tiempo_empleado_inicial=time.time()
+    lista.append(puzzle)
+    print(lista)
+    mov=0
+    i=-1
+    while 1:
+        i+=1
+        nodo=lista[i].copy()
+        indice=nodo.index(0)
+        print("MOVIMIENTOS DE NODO: \n")
+        print(f'{nodo}')
+        puzzle_izquierda=izquierda(nodo.copy(), indice)
+        if puzzle_izquierda is not None and puzzle_izquierda not in lista:
+            print(puzzle_izquierda)
+            lista.append(puzzle_izquierda)
+            mov+=1
+            if puzzle_izquierda==correcto:
+                break
+        puzzle_derecha=derecha(nodo.copy(), indice)
+        if puzzle_derecha is not None and puzzle_derecha not in lista:
+            print(puzzle_derecha)
+            lista.append(puzzle_derecha)
+            mov+=1
+            if puzzle_derecha==correcto:
+                break
+        puzzle_arriba=arriba(nodo.copy(), indice)
+        if puzzle_arriba is not None and puzzle_arriba not in lista:
+            print(puzzle_arriba)
+            lista.append(puzzle_arriba)
+            mov+=1
+            if puzzle_arriba==correcto:
+                break
+        puzzle_abajo=abajo(nodo.copy(), indice)
+        if puzzle_abajo is not None and puzzle_abajo not in lista:
+            print(puzzle_abajo)
+            lista.append(puzzle_abajo)
+            mov+=1
+            if puzzle_abajo==correcto:
+                break
+    print("Movimientos necesarios para llegar a la solucion: \n")
+    print(f'{mov}')
+    tiempo_empleado_final=time.time()
+    print("Tiempo requerido: \n")
+    print(f'{tiempo_empleado_inicial-tiempo_empleado_inicial} s')
+
+def busqueda_random(puzzle, correcto):
+    tiempo_empleado_inicial=time.time()
+    print("Puzzle inicial: \n")
+    print(f'{puzzle}')
+    mov=0
+    while True:
+        mix_p=mezcla(puzzle)
+        puzzle=mix_p
+        mov+=1
+        if puzzle==correcto:
+            print("Movimientos necesarios para llegar a la solucion: \n")
+            print(f'{mov}')
+            print("Puzzle final: \n")
+            print(f'{puzzle}')
+            break
+    tiempo_empleado_final=time.time()
+    print("Tiempo requerido: \n")
+    print(f'{tiempo_empleado_inicial-tiempo_empleado_inicial} s')
+
+#y para finalizar definimos el main
+
+def main():
+    puzzle=[0, 1, 2, 3, 4, 5, 6, 7, 8]
+    og=puzzle
+    correcto=[1, 2, 3, 4, 5, 6, 7, 8, 0]
+
+    while True:
+        algoritmo = menu_usuario(puzzle)
+        if algoritmo==1:
+            for i in range(50):
+                puzzle=mezcla(puzzle)
+        elif algoritmo==2:
+            busqueda_random(puzzle, correcto)
+        elif algoritmo==3:
+            anchura(puzzle, correcto)
+        elif algoritmo==4:
+            break
+        else:
+            print("Seleccion erronea")
+
+if __name__ == '__main__':
+    
+    main()
+        
+
+
+
+        
+
+    
--- tp1/ej.py
+++ tp1/ej.py
@@ -1,63 +1,63 @@
-import os
-import time
 import random
+import time
+import os
 
-lista_inicial = []
 
-#a continuacion se procede a definir los diferentes
-#tipos de movimientos posibles
-#derecha, izquierda, arriba y abajo
+visitados = []
 
-def der(puzzle, indexado):
-   if indexado % 3 < 2:
-      respaldo = puzzle[indexado + 1]
-      puzzle[indexado + 1] = puzzle[indexado]
-      puzzle[indexado] = respaldo
-      return puzzle
-      
-def izq(puzzle, indexado):
-   if indexado % 3 > 0:
-      respaldo = puzzle[indexado - 1]
-      puzzle[indexado - 1] = puzzle[indexado]
-      puzzle[indexado] = respaldo
-      return puzzle
-
-def arriba(puzzle, indexado):
-   if indexado - 3 >= 0:
-      respaldo = puzzle[indexado - 3]
-      puzzle[indexado - 3] = puzzle[indexado]
-      puzzle[indexado] = respaldo
+def moveUp(puzzle, ind):
+   if ind - 3 >= 0:
+      aux = puzzle[ind - 3]
+      puzzle[ind - 3] = puzzle[ind]
+      puzzle[ind] = aux
       return puzzle
 
 
-def abajo(puzzle, indexado):
-   if indexado + 3 < len(puzzle):
-      respaldo = puzzle[indexado + 3]
-      puzzle[indexado + 3] = puzzle[indexado]
-      puzzle[indexado] = respaldo
+def moveDown(puzzle, ind):
+   if ind + 3 < len(puzzle):
+      aux = puzzle[ind + 3]
+      puzzle[ind + 3] = puzzle[ind]
+      puzzle[ind] = aux
       return puzzle
 
 
-def shuffle(puzzle, indexado):
+def moveLeft(puzzle, ind):
+   if ind % 3 > 0:
+      aux = puzzle[ind - 1]
+      puzzle[ind - 1] = puzzle[ind]
+      puzzle[ind] = aux
+      return puzzle
+
+
+def moveRight(puzzle, ind):
+   if ind % 3 < 2:
+      aux = puzzle[ind + 1]
+      puzzle[ind + 1] = puzzle[ind]
+      puzzle[ind] = aux
+      return puzzle
+
+
+def shuffle(puzzle):
    func_list = [0, 1, 2, 3]
-   while True:
-      selected_func = random.choice(func_list)
-      if selected_func == 0:
-         if indexado - 3 >= 0:
-               puzzle = moveUp(puzzle, indexado)
-               break
-      elif selected_func == 1:
-         if indexado + 3 < len(puzzle):
-               puzzle = moveDown(puzzle, indexado)
-               break
-      elif selected_func == 2:
-         if indexado % 3 > 0:
-               puzzle = moveLeft(puzzle, indexado)
-               break
-      elif selected_func == 3:
-         if indexado % 3 < 2:
-               puzzle = moveRight(puzzle, indexado)
-               break
+   # while True:
+   ind = puzzle.index(0)
+   selected_func = random.choice(func_list)
+   if selected_func == 0:
+      if ind - 3 >= 0:
+            puzzle = moveUp(puzzle, ind)
+            # break
+   elif selected_func == 1:
+      if ind + 3 < len(puzzle):
+            puzzle = moveDown(puzzle, ind)
+            # break
+   elif selected_func == 2:
+      if ind % 3 > 0:
+            puzzle = moveLeft(puzzle, ind)
+            # break
+   elif selected_func == 3:
+      if ind % 3 < 2:
+            puzzle = moveRight(puzzle, ind)
+            # break
    return puzzle
 
 
@@ -79,15 +79,6 @@ def menu(puzzle):
    return selected
 
 
-def mezclar(puzzle):
-   # mezclo con 50 movimientos
-   for _ in range(50):
-      ind = puzzle.index(0)
-      s_puzzle = shuffle(puzzle, ind)
-      puzzle = s_puzzle
-   return puzzle
-
-
 def random_solver(puzzle, solucion):
    # inicio el conteo de tiempo
    inicio = time.time()
@@ -96,8 +87,8 @@ def random_solver(puzzle, solucion):
    print(f'Estado inicial: {puzzle}')
    movimientos = 0
    while True:
-      ind = puzzle.index(0)
-      s_puzzle = shuffle(puzzle, ind)
+      # ind = puzzle.index(0)
+      s_puzzle = shuffle(puzzle)
       puzzle = s_puzzle
       movimientos += 1
       if puzzle == solucion:
@@ -122,10 +113,10 @@ def bidireccional(puzzle_original, puzzle):
    print(f'Estado inicial: {puzzle}')
    movimientos = 0
    while True:
-      ind_o = puzzle_original.index(0)
-      ind = puzzle.index(0)
-      o_puzzle = shuffle(puzzle_original, ind_o)
-      s_puzzle = shuffle(puzzle, ind)
+      # ind_o = puzzle_original.index(0)
+      # ind = puzzle.index(0)
+      o_puzzle = shuffle(puzzle_original)
+      s_puzzle = shuffle(puzzle)
       puzzle_original = o_puzzle
       puzzle = s_puzzle
       movimientos += 1
@@ -198,7 +189,8 @@ def main():
    while True:
       selected = menu(puzzle)
       if selected == 0:
-         puzzle = mezclar(puzzle)
+         for i in range(50):
+            puzzle = shuffle(puzzle)
       elif selected == 1:
          random_solver(puzzle, solucion)
       elif selected == 2:
